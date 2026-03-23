40c59d646aacc3a53ce793ca70a9c7f69e1c6753
Ramkrishna Sharma <philosophicalbuilder@gmail.com>
2026-03-23 17:52:54 -0400

    PA3

--- PA3.java
+++ PA3.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class PA3 {
 
     public static void main(String[] args) {
         // pa3
+
+        Scanner stdin = new Scanner(System.in);
+        int test_cases = stdin.nextInt();
+
+        for (int t = 1; t <= test_cases; t++) {
+            int x = stdin.nextInt();
+            int y = stdin.nextInt();
+        }
     }
 
 }
