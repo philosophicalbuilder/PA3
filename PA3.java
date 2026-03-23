6ec00d9ca5d898294b08a5c0517ad2c86cddc195
Ramkrishna Sharma <philosophicalbuilder@gmail.com>
2026-03-23 17:58:57 -0400

    update

--- PA3.java
+++ PA3.java
@@ -11,7 +11,22 @@ public class PA3 {
         for (int t = 1; t <= test_cases; t++) {
             long x = stdin.nextLong();
             long y = stdin.nextLong();
+            int z = stdin.nextInt();
         }
+
+        /*
+         * 
+         * need to store all the names and costs because we can't print thenm right away
+         * we have to sort by cost first - if we printed as we went
+         * they'd be in input order instead of sorted.
+         * So have paraellel arrays just hold everything uinitl we're ready to
+         * sort/print.
+         * 
+         * 
+         */
+        String[] names = new String[z];
+        long[] costs = new long[z];
+
     }
 
 }
