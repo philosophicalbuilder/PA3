adff102ed4f243754fa8b9b3e12a656431e60ce7
Ramkrishna Sharma <philosophicalbuilder@gmail.com>
2026-03-24 04:36:40 -0400

    update

--- PA3.java
+++ PA3.java
@@ -20,10 +20,10 @@ public class PA3 {
                 names[i] = stdin.next();
                 long removeCost = stdin.nextLong();
                 long halveCost = stdin.nextLong();
- 
+
                 int numHalves = 0;
                 long val = x;
- 
+
                 while (val / 2 >= y) {
                     long killed = val - val / 2;
                     if (halveCost <= killed * removeCost) {
@@ -33,29 +33,34 @@ public class PA3 {
                         break;
                     }
                 }
+            }
+
+            /*
+             * 
+             * need to store all the names and costs because we can't print thenm right away
+             * we have to sort by cost first - if we printed as we went
+             * they'd be in input order instead of sorted.
+             * So have paraellel arrays just hold everything uinitl we're ready to
+             * sort/print.
+             * 
+             * 
+             */
+
+            // for each company:
+            // 1. read in the name, remove cost, and halve cost
+            // 2. start at x zombies
+            // 3. keep halving as long as we stay above y
+            // - if halving is cheaper than removing that many one by one, halve
+            // - otherwise stop halving
+            // 4. cost = (number of halves * halve cost) + (remaining zombies * remove cost)
+            // // is remaining zombies right?
+            // it should be (val - y) not just val, we only need to remove down to y
+
+        }
+        public int sortByResult(String[] naes, long[]costs, int a, int b) {
+
+            
         }
 
-        /*
-         * 
-         * need to store all the names and costs because we can't print thenm right away
-         * we have to sort by cost first - if we printed as we went
-         * they'd be in input order instead of sorted.
-         * So have paraellel arrays just hold everything uinitl we're ready to
-         * sort/print.
-         * 
-         * 
-         */
-
-        // for each company:
-        // 1. read in the name, remove cost, and halve cost
-        // 2. start at x zombies
-        // 3. keep halving as long as we stay above y
-        // - if halving is cheaper than removing that many one by one, halve
-        // - otherwise stop halving
-        // 4. cost = (number of halves * halve cost) + (remaining zombies * remove cost)
-        // // is remaining zombies right?
-        // it should be (val - y) not just val, we only need to remove down to y
-
     }
-
 }
